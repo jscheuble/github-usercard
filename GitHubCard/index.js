ba8756ce35ead2d22989e48fb3af75f746411251
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-02-13 13:04:56 -0800

    steps 1-3 complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,15 @@
            https://api.github.com/users/<your name>
 */
 
+// axios.get("https://api.github.com/users/jscheuble")
+//   .then(response => {
+//     console.log(response.data);
+//     parent.append(cardMaker(response.data));
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -53,3 +62,39 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+function cardMaker(obj) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  const avatar = document.createElement('img');
+  avatar.src = obj.avatar_url;
+  const info = document.createElement('div');
+  info.classList.add('card-info');
+  const name = document.createElement('h3');
+  name.classList.add('name');
+  name.textContent = obj.name;
+  const username = document.createElement('p');
+  username.classList.add('username');
+  username.textContent = obj.login;
+  const location = document.createElement('p');
+  location.textContent = `Location: ${obj.location}`;
+  const profile = document.createElement('p');
+  profile.textContent = 'Profile: ';
+  const profileLink = document.createElement('a');
+  profileLink.href = obj.html_url;
+  profileLink.textContent = obj.html_url;
+  const followers = document.createElement('p');
+  followers.textContent = `Followers: ${obj.followers}`;
+  const following = document.createElement('p');
+  following.textContent = `Following: ${obj.following}`;
+  const bio = document.createElement('p');
+  bio.textContent = `Bio: ${obj.bio}`;
+
+  profile.append(profileLink);
+  info.append(name, username, location, profile, followers, following, bio);
+  card.append(avatar, info);
+
+  return card;
+}
+
+const parent = document.querySelector('.cards');
